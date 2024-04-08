bf7d52d69222031e99ab6cfb247e43e69b829f1f
bitsZERO <2854834213@qq.com>
2024-04-08 23:40:53 +0800

    Site updated: 2024-04-08 23:40:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/05/hello-world/","2024/04/05/day01 编程基础/","2024/04/05/test/","2024/04/07/image/"];function toRandomPost(){
+var posts=["2024/04/07/image/","2024/04/08/我的第一篇博客文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
